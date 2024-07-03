984c194a2825facfa1f6072de72d99738d464b98
Zohir <vasv29834@gmail.com>
2024-07-03 18:24:37 +0500

    first commit

---
+++ src/components/projects.tsx
@@ -0,0 +1,5 @@
+function Projects() {
+  return <div className="w-6/12 h-full bg-stone-950">projects</div>;
+}
+
+export default Projects;
---
+++ src/components/todos.tsx
@@ -0,0 +1,7 @@
+function Todos() {
+  return (
+    <div className="w-6/12 bg-white text-black rounded-lg h-full">todos</div>
+  );
+}
+
+export default Todos;
