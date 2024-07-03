40670f19f50e9f8a200a255bd3ed106856c283ec
Zohir <vasv29834@gmail.com>
2024-07-04 01:12:44 +0500

    Todos block header

--- src/components/project_item.tsx
+++ src/components/project_item.tsx
@@ -1,7 +1,7 @@
 function Project_item() {
   return (
     <div className="w-full h-40 flex justify-center items-center ">
-      <div className="w-32 h-32 bg-white text-black font-bold text-xl rounded-full flex justify-center items-center">
+      <div className="w-32 h-32 bg-white text-black font-bold text-xl rounded-xl flex justify-center items-center">
         MT
       </div>
     </div>
--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -1,6 +1,23 @@
+import { Icon } from "@iconify/react";
+
 function Todos() {
   return (
-    <div className="w-6/12 bg-white text-black rounded-lg h-full">todos</div>
+    <div className="w-6/12 bg-white text-black rounded-lg h-full flex justify-center items-center">
+      <section className="w-3/4 h-4/5 ">
+        <header className="flex justify-between">
+          <div>
+            <p className="text-xl font-semibold">Math Tasks</p>
+            <p className="text-gray-500 font-medium">We should learn math</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <p className="text-gray-500 font-medium">Add Members</p>
+            <button className="users w-14 h-14 flex justify-center items-center hover:bg-purple-600 hover:text-white duration-100 bg-slate-200 rounded-lg">
+              <Icon icon="ic:baseline-plus" width={25} />
+            </button>
+          </div>
+        </header>
+      </section>
+    </div>
   );
 }
 
